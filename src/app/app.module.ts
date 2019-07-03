77b6d7e0287fcc280a4f96b96293792ac43bfc5d
Nick Lersberghe <Nick.Lersberghe@codit.eu>
2019-07-03 15:52:13 +0200

    Added components, added AppState, imports added to app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { StoreModule } from '@ngrx/store';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ShowcaseComponent } from './showcase/showcase.component';
+import { filterReducer } from './reducers/filter.reducer';
+import { ReadComponent } from './components/read/read.component';
+import { CreateComponent } from './components/create/create.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShowcaseComponent,
+    ReadComponent,
+    CreateComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    StoreModule.forRoot({
+      filter: filterReducer
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
