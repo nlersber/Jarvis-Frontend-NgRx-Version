0b9d63ad1089c66c68b32ec18cf0e6cbcf8933d9
Nick Lersberghe <Nick.Lersberghe@codit.eu>
2019-07-04 09:09:12 +0200

    Revert "Added components, added AppState, imports added to app.module"
    This reverts commit 77b6d7e0287fcc280a4f96b96293792ac43bfc5d.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { StoreModule } from '@ngrx/store';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ShowcaseComponent } from './showcase/showcase.component';
-import { filterReducer } from './reducers/filter.reducer';
-import { ReadComponent } from './components/read/read.component';
-import { CreateComponent } from './components/create/create.component';
-
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ShowcaseComponent,
-    ReadComponent,
-    CreateComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    StoreModule.forRoot({
-      filter: filterReducer
-    })
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
